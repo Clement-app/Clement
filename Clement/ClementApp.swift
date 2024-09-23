aec83d70d59da51c5039f0e518087a600b10a8f0
Alex Catchpole <alex.catchpole@hoppa.com>
2024-09-23 15:43:47 +0100

    Initial Commit

---
+++ Clement/ClementApp.swift
@@ -0,0 +1,32 @@
+//
+//  ClementApp.swift
+//  Clement
+//
+//  Created by Alex Catchpole on 23/09/2024.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct ClementApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
